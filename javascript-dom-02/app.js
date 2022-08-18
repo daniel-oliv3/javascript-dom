09e0f63756a0d7264c076b51fbb0df3d2fbe2f59
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-18 10:29:38 -0300

    Update

--- javascript-dom-02/app.js
+++ javascript-dom-02/app.js
@@ -1,8 +1,32 @@
 
 
-//Função
+//Função saudação
 function saudacao(){
     alert('Olá JavaScript!');
 }
 
-window.saudacao();
+//Função largura da tela
+function larguraTela(){
+    alert(window.innerWidth);
+}
+
+
+
+
+
+//alterando o background do body
+document.body.style.background = "#1C1C1C";
+
+//alterando a cor do texto
+document.body.style.color = "#F0F8FF";
+
+
+//alterando o titulo pelo id
+//document.getElementById('titulo').innerText = "Novo titulo!";
+
+//============================ 
+
+//window.saudacao();
+//saudacao();
+
+//larguraTela();
