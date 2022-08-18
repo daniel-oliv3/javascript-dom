872cc0f52404909833fcf3707a879a9548b75f43
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-18 15:02:04 -0300

    Update

--- javascript-dom-03/app.js
+++ javascript-dom-03/app.js
@@ -3,5 +3,5 @@ console.log(document.body.childNodes);
 document.body.childNodes[1].innerText = "Novo texto do parágrafo";
 document.body.childNodes[1].textContent = "Novo texto do parágrafo 2";
 
-
+console.log(document.documentElement.lang);
 
