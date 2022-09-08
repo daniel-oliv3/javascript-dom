0f43a92d7cbc920b91ed8aed838a56701a97b497
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-08 17:32:57 -0300

    Update

--- javascript-dom-08/script.js
+++ javascript-dom-08/script.js
@@ -1,8 +1,19 @@
 
 //* ======================================================
+
 //Usando funções e eventos para alterar o conteúdo
+/*function executar(){
+    document.querySelector("#titulo").innerText = "Novo Titulo!";
+}
+
+//executar();*/
 
 
+function executar(){
+    let e = document.querySelector("#titulo");
+    e.innerText = "Novo Titulo!";
+    e.getElementsByClassName.color = "blue";
+}
 
 
 
