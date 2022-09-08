b94bd8df0f6674f3e3b67961cb179dfdabf71a3b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-08 16:03:36 -0300

    Update

--- javascript-dom-07/script.js
+++ javascript-dom-07/script.js
@@ -1,33 +1,8 @@
-//1* ======================================================
-//let elemento = document.querySelector('#titulo');
-//let elemento = document.querySelector('.noticia');
-//let elemento = document.querySelector('div');
-//let elemento = document.querySelectorAll('div');
 
-/*alterando a cor do texto com javascript*/
-//let elemento = document.querySelector('#titulo > i > u');
-//elemento.style.color = "gold";
-
-
-//alterando a cor de fundo
-//elemento.style.backgroundColor = "blue";
-
-//console.log(elemento);
-/**/
-
-
-
-//2* ======================================================
+//* ======================================================
 let elemento = document.querySelector(".noticia > h1");
 
-
-//console.log(elemento.innerText);
-//console.log(elemento.innerHTML);
-
-//alterando o texto
-//elemento.innerText = "Sapup3 na área!"; //apresenta no console o texto junto com a tag <u></u>
-
-elemento.innerText = "Sapup3 na <u>área!</u>"; //apresenta no console o texto sem tag <u></u>
+elemento.outerHTML = "<p>Novo Titulo</p>";
 
 console.log(elemento.outerHTML);
 
