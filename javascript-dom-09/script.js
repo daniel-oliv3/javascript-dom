27a47196dc4363711471765e363e0b8a064d0acb
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-08 17:52:18 -0300

    Clear project and update

--- javascript-dom-09/script.js
+++ javascript-dom-09/script.js
@@ -1,31 +1,6 @@
 
 //* ======================================================
 
-//Usando funções e eventos para alterar o conteúdo
-/*function executar(){
-    document.querySelector("#titulo").innerText = "Novo Titulo!";
-}
-
-//executar();*/
-
-
-/*function executar(){
-    let e = document.querySelector("#titulo");
-    e.innerText = "Novo Titulo!";
-    e.style.color = "blue";
-}*/
-
-
-//alterando o texto ao clicar na imagem
-function executar(){
-    let e = document.querySelector("#titulo");
-    e.innerText = "Novo Titulo!";
-    e.style.color = "blue";
-}
-
-
-
-
 
 
 /**/
