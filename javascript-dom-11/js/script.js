305194fb83af710593a8377f7ee1743a6940de16
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-09 14:08:04 -0300

    Create project Document Object Model number 11

---
+++ javascript-dom-11/js/script.js
@@ -0,0 +1,58 @@
+/*1==================================================*/
+
+function executar(){
+    //cria o elemento
+    let novo_elemento = document.createElement('p');
+    novo_elemento.className = "alerta";
+    novo_elemento.innerText = "Esta é uma mensagem de alerta!";
+
+
+    //adicionar o novo elemento ao div existente
+    let elemento = document.querySelector(".caixa");
+    elemento.appendChild(novo_elemento);
+}
+
+
+/*2==================================================*/
+
+/*
+    //cria o elemento
+    let novo_elemento = document.createElement('p');
+    novo_elemento.className = "alerta";
+    novo_elemento.innerText = "Esta é uma mensagem de alerta!";
+
+
+    //adicionar o novo elemento ao div existente
+    let elemento = document.querySelector(".caixa");
+    elemento.appendChild(novo_elemento);
+    elemento.appendChild(novo_elemento.cloneNode(true));
+    elemento.appendChild(novo_elemento.cloneNode(true));
+    elemento.appendChild(novo_elemento.cloneNode(true));
+    elemento.appendChild(novo_elemento.cloneNode(true));
+
+*/
+
+
+
+
+
+
+
+/**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
