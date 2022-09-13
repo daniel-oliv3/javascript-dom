b2377200114451f16586a46dffb8c8b1fec3753d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-13 12:04:08 -0300

    Update script.js

--- javascript-dom-11/js/script.js
+++ javascript-dom-11/js/script.js
@@ -2,7 +2,7 @@
 
 let div = document.querySelector('.container');
 
-
+/*
 //Adicionando texto antes da div
 div.before('AAAA');
 
@@ -16,12 +16,26 @@ div.append('CCCCC');
 //Adicionando depois da div
 div.after('DDDDD');
 
+*/
+
+let p1 = document.createElement('p');
+let p2 = document.createElement('p');
+let p3 = document.createElement('p');
+let p4 = document.createElement('p');
+p1.innerText = 'Parágrafo 1';
+p2.innerText = 'Parágrafo 2';
+p3.innerText = 'Parágrafo 3';
+p4.innerText = 'Parágrafo 4';
 
 
+//colocar o paragrafo antes do div
+div.before(p1);
 
+div.prepend(p2);
 
+div.append(p3);
 
-
+div.after(p4);
 
 
 
